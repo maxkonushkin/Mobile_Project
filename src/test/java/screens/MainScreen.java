50278eaaf8835bc7c88fb1591de6377b05ad8239
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-06-02 11:32:20 +0400

    Add MainScreen

--- src/test/java/screens/MainScreen.java
+++ src/test/java/screens/MainScreen.java
@@ -1,4 +1,4 @@
-package pages;
+package screens;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
-public class MainPage {
+public class MainScreen {
 
     private final SelenideElement checkResult = $(byXpath("//android.widget.TextView")),
             сatalogScreen = $(byXpath("//android.view.View[@resource-id='CatalogScreen']/android.view.View")),
@@ -16,31 +16,31 @@ public class MainPage {
             InputPhoneScreen = $(byXpath("//android.view.View[@resource-id='InputPhoneScreen']/android.view.View"));
 
     @Step("Нажимаем на кнопку Каталог")
-    public MainPage сatalogScreen() {
+    public MainScreen сatalogScreen() {
         сatalogScreen.click();
         return this;
     }
 
     @Step("Нажимаем на кнопку Карта")
-    public MainPage NearMeScreen() {
+    public MainScreen NearMeScreen() {
         NearMeScreen.click();
         return this;
     }
 
     @Step("Нажимаем на кнопку О банке")
-    public MainPage AboutBankScreen() {
+    public MainScreen AboutBankScreen() {
         AboutBankScreen.click();
         return this;
     }
 
     @Step("Нажимаем на кнопку Вход")
-    public MainPage InputPhoneScreen() {
+    public MainScreen InputPhoneScreen() {
         InputPhoneScreen.click();
         return this;
     }
 
     @Step("Проверяем результат")
-    public MainPage checkResult(String value) {
+    public MainScreen checkResult(String value) {
         checkResult.shouldHave(text(value));
         return this;
     }
--- src/test/java/tests/local/SearchTests.java
+++ src/test/java/tests/local/SearchTests.java
@@ -2,39 +2,39 @@ package tests.local;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pages.MainPage;
+import screens.MainScreen;
 
 public class SearchTests extends TestBase {
 
-    MainPage mainPage = new MainPage();
+    MainScreen mainScreen = new MainScreen();
 
     @Test
     @DisplayName("Проверка работы кнопки Каталог в нижнем тапбаре страницы регистрации")
     public void successfulClickingCatalogButtonTest() {
-        mainPage.сatalogScreen();
-        mainPage.checkResult("Каталог");
+        mainScreen.сatalogScreen();
+        mainScreen.checkResult("Каталог");
     }
 
     @Test
     @DisplayName("Проверка работы кнопки Карта в нижнем тапбаре страницы регистрации")
     public void successfulClickingMapButtonTest() {
-        mainPage.NearMeScreen();
-        mainPage.checkResult("Зайдите в Профиль приложения, чтобы посмотреть все пункты обслуживания или откройте общий список на сайте");
+        mainScreen.NearMeScreen();
+        mainScreen.checkResult("Зайдите в Профиль приложения, чтобы посмотреть все пункты обслуживания или откройте общий список на сайте");
     }
 
     @Test
     @DisplayName("Проверка работы кнопки О банке в нижнем тапбаре страницы регистрации")
     public void successfulClickingBankButtonTest() {
-        mainPage.AboutBankScreen();
-        mainPage.checkResult("О банке");
+        mainScreen.AboutBankScreen();
+        mainScreen.checkResult("О банке");
     }
 
     @Test
     @DisplayName("Проверка работы кнопки Вход в нижнем тапбаре страницы регистрации")
     public void successfulClickingLoginButtonTest() {
-        mainPage.AboutBankScreen();
-        mainPage.InputPhoneScreen();
-        mainPage.checkResult("Введите номер телефона");
+        mainScreen.AboutBankScreen();
+        mainScreen.InputPhoneScreen();
+        mainScreen.checkResult("Введите номер телефона");
     }
 
 }
